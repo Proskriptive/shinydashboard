3fb6eca1753426f0a31d1adddb04ac0716e3467c
PC-32\pc14 <jatinderbahl@gmail.com>
2017-03-22 12:50:23 +0530

    Reproduce the capabilities on the left sidebar to the right side control bar.

--- R/dashboardControlbar.R
+++ R/dashboardControlbar.R
@@ -3,6 +3,7 @@
 #' TODO: create functions that can create sub-components in control-sidebar,
 #' like functions defined in dashboardSidebar.R
 #'
+#'@param ... Items to put in the sidebar.
 #' @export
 dashboardControlbar <- function(ctrlHTML = NULL) {
 
@@ -10,11 +11,13 @@ dashboardControlbar <- function(ctrlHTML = NULL) {
 
     HTML(paste0(
       '<!-- Control Sidebar -->
+
       <aside class="control-sidebar control-sidebar-dark">
       <!-- Create the tabs -->
       <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
       <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
       <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+      <li><a href="#control-sidebar-theme-demo-options-tab" data-toggle="tab"><i class="fa fa-wrench"></i></a></li>
       </ul>
       <!-- Tab panes -->
       <div class="tab-content">
@@ -88,11 +91,55 @@ dashboardControlbar <- function(ctrlHTML = NULL) {
       Some information about this general settings option
       </p>
       </div>
+      <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Allow mail redirect
+              <input type="checkbox" class="pull-right" checked="">
+            </label>
+
+            <p>
+              Other sets of options are available
+            </p>
+      </div>
       <!-- /.form-group -->
+      <h3 class="control-sidebar-heading">Chat Settings</h3>
+      <div class="form-group">
+            <label class="control-sidebar-subheading">
+      Show me as online
+      <input type="checkbox" class="pull-right" checked="">
+      </label>
+      </div>
+      <div class="form-group">
+      <label class="control-sidebar-subheading">
+      Turn off notifications
+      <input type="checkbox" class="pull-right">
+      </label>
+      </div>
+      <div class="form-group">
+      <label class="control-sidebar-subheading">
+      Delete chat history
+      <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
+      </label>
+      </div>
+
       </form>
       </div>
       <!-- /.tab-pane -->
-      <div class= "tab-pane" id="control-sidebar-theme-demo-options-tab"
+      <div class= "tab-pane" id="control-sidebar-theme-demo-options-tab">
+      <div>
+      <h4 class = "control-sidebar-subheading">Layout Options</h4>
+      <div class = "form-group">
+      <label class="control-sidebar-subheading"><input type="checkbox" data-layout="fixed" class="pull-right"> Fixed layout</label>
+      <p>Activate the fixed layout. You can not use fixed and boxed layouts together</p>
+      </div>
+      <div class = "form-group">
+      <label class="control-sidebar-subheading"><input type="checkbox" data-layout="sidebar-collapse" class="pull-right"> Toggle Sidebar</label>
+      <p>Toggle the left sidebar state (open or collapse)</p>
+      </div>
+      <div class = "form-group">
+      <label class="control-sidebar-subheading"><input type="checkbox" data-layout="sidebar-collapse" class="pull-right"> Toggle Sidebar</label><p>Toggle the left sidebar state (open or collapse)</p>
+      </div>
+      </div>
       </div>
       </aside>
       <!-- /.control-sidebar -->
@@ -107,3 +154,136 @@ dashboardControlbar <- function(ctrlHTML = NULL) {
 
   }
 }
+
+#' Create a dashboard control Right sidebar.
+#'
+#' TODO: create functions that can create sub-components in control-sidebar,
+#' like functions defined in dashboardSidebar.R
+#'
+#' @param ... Items to put in the Control Bar.
+#' @param paneldivs Divs for nav tabs.
+#'
+#' @export
+CreateControlBarRight<-function(..., paneldivs = NULL)
+{
+  items <- list(...)
+
+  Tabdiv <- list(paneldivs)
+  lapply(Tabdiv, tagAssert,type = "div")
+  lapply(items, tagAssert, type = "li")
+  tags$div(class = "Control-Bar Area",
+           tags$aside(class = "control-sidebar control-sidebar-dark",tags$ul(class = "nav nav-tabs nav-justified control-sidebar-tabs",items)
+                      ,Tabdiv),
+           tags$div(class = "control-sidebar-bg")
+           )
+
+}
+
+#' Create Nav tabs on the Shiny Dashboard Control Sidebar.
+#'
+#' The tabs can contain tab ID which will be specific to display that tab Content in the div
+#' below it
+#'
+#' @param tabID Shiny Input ID for the Tab anchor link
+#' @param icon An icon tag, created by \code{\link[shiny]{icon}}.
+
+#' @export
+CreateNavTabs <- function(tabID ,icon = NULL)
+{
+  if(!is.null(icon)) tagAssert(icon,type = "i")
+
+     tags$li(tags$a(href = paste0("#control-sidebar-",tabID),`data-toggle` = "tab",icon)
+     )
+
+
+}
+
+
+#' Creates the settings Panel Div for the Nav Tabs in the Dashboard Control Bar.
+#'
+#' It will contain the Tab Panel Items Specific to the General Settings Tab.
+#'
+#' @param ... Form Items to Put in the Settings Tab Content Panel.
+#' @param panelHeading panel heading name
+#' @export
+CreateSettingsTabPanel <- function(...,panelHeading)
+{
+  FormPanel <- list(...)
+  lapply(FormPanel, tagAssert, type = "div")
+  if( panelHeading == "General Settings")
+  {
+    tags$div(class = "tab-Setting",tags$form(
+      method = "post",tags$h3(class = "control-sidebar-heading",panelHeading),
+      FormPanel
+    ))
+  }
+  else
+  {
+    tags$div(class = "tab-chatSetting",tags$form(
+      method = "post",tags$h3(class = "control-sidebar-heading",panelHeading),
+      FormPanel
+    ))
+  }
+
+
+}
+
+#' Creates the Home Panel Div for the Nav Tabs in the Dashboard Control bar.
+#'
+#' It will contain the Tab Panel Items Specific to the Home Tab.
+#'
+#' @param  ... List Items to Put in the Home Tab
+#' @param panelHeading panel heading name
+#'
+#' @export
+CreateHomeTabPanel <- function(...,panelHeading)
+{
+  ListItems <- list(...)
+  lapply(ListItems, tagAssert, type = "li")
+  tags$div(class = "tab-pane active", id = "control-sidebar-home-tab",tags$ul(class = "control-sidebar-menu",tags$h3(class = "control-sidebar-heading",panelHeading),ListItems))
+}
+
+#' Creates the list items to be embeddded into the Home Tab Panel Content.
+#'
+#' It will be used to render the items present in the Home tab.
+#'
+#' @param Header This will contain the header text for the list element.
+#' @param ProgressValue This value moves the Progress bar.
+#' @param ProgressBarClass This is used to set the Bootstrap Class for displaying the Progress Bar.
+#'
+#' @export
+   CreateListItems <- function(Header,ProgressValue,ProgressBarClass)
+   {
+      if(ProgressBarClass %in% validStatuses)
+      {
+        tags$li(a(href = "javascript:void(0)",tags$h4(class = "control-sidebar-subheading",Header,tags$span(class = paste0("label label-",ProgressBarClass," pull-right"),ProgressValue)),
+                  tags$div(class = "progress progress-xxs",tags$div(class = paste0("progress-bar progress-bar-",ProgressBarClass),style = paste0("width:",ProgressValue,"%")))))
+      }
+   }
+
+
+
+#' Creates the Form for the settings Panel which consists of checkboxes in the dashboard Control bar.
+#'
+#' It will contain Form to be processed by user
+#'
+#' @param Header This will contain the header text for the form element.
+#' @param Description This Provides the description for the setting element.
+#' @param icon This will append icon to the Settings Element.
+#'
+#' @export
+  CreateFormPanel <- function(Header,Description,icon = NULL)
+  {
+     if(!is.null(icon))
+     {
+       tagAssert(icon, type = "i")
+       tags$div(class = "form-group",tags$label(class = "control-sidebar-subheading", Header,a(href = "javascript:void(0)", class = "text-red pull-right",icon)) )
+
+     }
+    else {
+      tags$div(class = "form-group",tags$label(class = "control-sidebar-subheading", Header,tags$input(type = "checkbox", class = "pull-right")),
+               tags$p(Description))
+    }
+
+
+  }
